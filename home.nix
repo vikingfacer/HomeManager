546cfc075a1713490df3cf6f93acf0c4aee34af1
JM <jacobmontpetit@gmail.com>
2022-12-24 17:09:21 -0500

    service:gnupg enable

--- home.nix
+++ home.nix
@@ -51,6 +51,11 @@
     extraConfig.core.editor = "vim";
   };
 
+  services.gpg-agent = {
+    enable = true;
+    enableSshSupport = true;
+  };
+
   # This value determines the Home Manager release that your
   # configuration is compatible with. This helps avoid breakage
   # when a new Home Manager release introduces backwards
